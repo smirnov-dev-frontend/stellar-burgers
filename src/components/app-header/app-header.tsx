ccfb4a288c288121ea41bf38f3941c16ed40a146
Alexander Smirnov <smirnov.dev.frontend@gmail.com>
2026-04-12 21:43:51 +0300

    feat: добавлена навигация и активные ссылки в шапке

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,11 @@
 import { FC } from 'react';
-import { AppHeaderUI } from '@ui';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../services/store';
+import { selectUser } from '../../services/selectors/userSelectors';
+
+export const AppHeader: FC = () => {
+  const user = useSelector(selectUser);
+
+  return <AppHeaderUI userName={user?.name || ''} />;
+};
--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react';
+import { NavLink, useMatch } from 'react-router-dom';
+
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 import {
@@ -8,28 +10,58 @@ import {
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
 
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
-      <div className={styles.menu_part_left}>
-        <>
-          <BurgerIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
-      </div>
-      <div className={styles.logo}>
-        <Logo className='' />
-      </div>
-      <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
-      </div>
-    </nav>
-  </header>
-);
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const isConstructorActive = Boolean(useMatch('/'));
+  const isFeedActive = Boolean(useMatch('/feed/*'));
+  const isProfileActive = Boolean(useMatch('/profile/*'));
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          <NavLink to='/' end className={styles.link}>
+            <BurgerIcon type={isConstructorActive ? 'primary' : 'secondary'} />
+            <p
+              className={`text text_type_main-default ml-2 mr-10 ${
+                isConstructorActive
+                  ? 'text_color_primary'
+                  : 'text_color_inactive'
+              }`}
+            >
+              Конструктор
+            </p>
+          </NavLink>
+
+          <NavLink to='/feed' className={styles.link}>
+            <ListIcon type={isFeedActive ? 'primary' : 'secondary'} />
+            <p
+              className={`text text_type_main-default ml-2 ${
+                isFeedActive ? 'text_color_primary' : 'text_color_inactive'
+              }`}
+            >
+              Лента заказов
+            </p>
+          </NavLink>
+        </div>
+
+        <NavLink to='/' className={styles.logo}>
+          <Logo className='' />
+        </NavLink>
+
+        <NavLink
+          to='/profile'
+          className={`${styles.link} ${styles.link_position_last}`}
+        >
+          <ProfileIcon type={isProfileActive ? 'primary' : 'secondary'} />
+          <p
+            className={`text text_type_main-default ml-2 ${
+              isProfileActive ? 'text_color_primary' : 'text_color_inactive'
+            }`}
+          >
+            {userName || 'Личный кабинет'}
+          </p>
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
